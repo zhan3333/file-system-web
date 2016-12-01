600bd23ca291e202f01e2399b59eaf44d15b7d54
zhanguang <zhanguang@ykxing.com>
2016-12-01 18:20:38 +0800

    修改页面结构，加入element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <home></home>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import home from './pages/home.vue'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    home
   }
 }
 </script>
